bf67166bcc0653203082553b96e4842961e97787
raghumad <raghunandan.madanala@yahoo.com>
2022-12-12 05:54:39 -0700

    fixing delete crash

--- Tern/Views/WayPointAnnotationCallout.swift
+++ Tern/Views/WayPointAnnotationCallout.swift
@@ -12,41 +12,41 @@ import MapKit
 struct WayPointAnnotationCallout : View {
     @EnvironmentObject var model : RoutePlannerModel
     @State var editWaypoint : Bool = false
-    let waypointIndex : Int
+    let waypoint : WayPoint // This has to be copy or delete will crash.
 
     var body : some View {
         VStack{
             HStack{
                 Image(systemName: "mappin.and.ellipse")
-                Text("\(String(format: "%.5f", model.waypoints[waypointIndex].coordinate.latitude)),\(String(format: "%.5f", model.waypoints[waypointIndex].coordinate.longitude))")
+                Text("\(String(format: "%.5f", waypoint.coordinate.latitude)),\(String(format: "%.5f", waypoint.coordinate.longitude))")
                 Spacer()
             }
             HStack {
                 Image(systemName: "cylinder")
-                Text("\(String(model.waypoints[waypointIndex].cylinderRadius.converted(to: .meters).value))m")
+                Text("\(String(waypoint.cylinderRadius.converted(to: .meters).value))m")
                 Image(systemName: "figure.climbing")
-                Text("\(String(format: "%.1f ft", model.waypoints[waypointIndex].elevation.converted(to: .feet).value))")
+                Text("\(String(format: "%.1f ft", waypoint.elevation.converted(to: .feet).value))")
                 Spacer()
             }
             HStack {
-                if model.waypoints[waypointIndex].weatherForecast.winddirection_80m.count > 0 {
+                if waypoint.weatherForecast.winddirection_80m.count > 0 {
                     Image(systemName: "arrow.up.circle")
-                        .rotationEffect(.degrees(model.waypoints[waypointIndex].weatherForecast.winddirection_80m[0].converted(to: .degrees).value))
+                        .rotationEffect(.degrees(waypoint.weatherForecast.winddirection_80m[0].converted(to: .degrees).value))
                 }
-                if model.waypoints[waypointIndex].weatherForecast.windspeed80m.count > 0 {
-                    Text("\(model.waypoints[waypointIndex].weatherForecast.windspeed80m[0].description)")
+                if waypoint.weatherForecast.windspeed80m.count > 0 {
+                    Text("\(waypoint.weatherForecast.windspeed80m[0].description)")
                 }
-                if model.waypoints[waypointIndex].weatherForecast.windgusts_10m.count > 0 {
+                if waypoint.weatherForecast.windgusts_10m.count > 0 {
                     Image(systemName: "wind.circle")
-                    Text("\(model.waypoints[waypointIndex].weatherForecast.windgusts_10m[0].description)")
+                    Text("\(waypoint.weatherForecast.windgusts_10m[0].description)")
                 }
-                if model.waypoints[waypointIndex].weatherForecast.relativehumidity_2m.count > 0 {
+                if waypoint.weatherForecast.relativehumidity_2m.count > 0 {
                     Image(systemName: "humidity")
-                    Text("\(model.waypoints[waypointIndex].weatherForecast.relativehumidity_2m[0].description)")
+                    Text("\(waypoint.weatherForecast.relativehumidity_2m[0].description)")
                 }
-                if model.waypoints[waypointIndex].weatherForecast.cloudcover.count > 0 {
+                if waypoint.weatherForecast.cloudcover.count > 0 {
                     Image(systemName: "cloud.circle")
-                    Text("\(model.waypoints[waypointIndex].weatherForecast.cloudcover[0].description)")
+                    Text("\(waypoint.weatherForecast.cloudcover[0].description)")
                 }
                 Spacer()
             }
@@ -55,7 +55,7 @@ struct WayPointAnnotationCallout : View {
             editWaypoint.toggle()
         }
         .sheet(isPresented: $editWaypoint) {
-            EditWaypoint(waypoint: model.waypoints[waypointIndex], editWaypoint: $editWaypoint, waypointName: model.waypoints[waypointIndex].title ?? "", latitude: model.waypoints[waypointIndex].coordinate.latitude, longitude: model.waypoints[waypointIndex].coordinate.longitude, cylinderRadius: model.waypoints[waypointIndex].cylinderRadius, waypointDescription: model.waypoints[waypointIndex].subtitle!).environmentObject(model)
+            EditWaypoint(waypoint: waypoint, editWaypoint: $editWaypoint, waypointName: waypoint.title ?? "", latitude: waypoint.coordinate.latitude, longitude: waypoint.coordinate.longitude, cylinderRadius: waypoint.cylinderRadius, waypointDescription: waypoint.subtitle!).environmentObject(model)
          .presentationDetents([.fraction(0.8)])
          .presentationDragIndicator(.visible)
          }
