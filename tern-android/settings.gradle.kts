63a9cb7ae4791028fd1464ee4b6a959e8a72fe44
Raghu Madanala <raghu@ternparagliding.com>
2025-08-23 14:03:02 -0600

    compiles for now.

--- tern-android/settings.gradle.kts
+++ tern-android/settings.gradle.kts
@@ -4,6 +4,12 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        kotlin("jvm") version "2.2.0"
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
     repositories {
